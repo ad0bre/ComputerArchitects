7cb6584306fdd01e1f789cf4e446c0d6ccb5496c
Andrei Dobre <andrei.dobr3@gmail.com>
2023-04-01 21:25:42 +0300

    Fixed Question?

--- Backend/Backend/Features/QuestionOptions/QuestionOptionsController.cs
+++ Backend/Backend/Features/QuestionOptions/QuestionOptionsController.cs
@@ -1,5 +1,7 @@
 using System.Net.Mime;
 using Backend.Database;
+using Backend.Features.QuestionOptions.Views;
+using Backend.Features.Questions;
 using Backend.Features.Questions.Views;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -18,10 +20,17 @@ public class QuestionOptionsController : ControllerBase
     {
         _dbContext = dbContext;
     }
-    
+
     // [HttpPost]
     // [Authorize(Roles = "manager")]
     // [ProducesResponseType(StatusCodes.Status201Created)]
-    // public async Task<ActionResult<QuestionResponse>> Post([])
+    // public async Task<ActionResult<QuestionOptionView>> Post([FromBody] QuestionOptionView request)
+    // {
+    //     var questionoption = new QuestionOption
+    //     {
+    //         QuestionId = request.QuestionId,
+    //         Label = request.Label
+    //     };
+    // }
 
 }
